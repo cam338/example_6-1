e85bdf28de15b1e1abc283b7851ffe1140b314fd
Chiharu Mamiya <mamiyac@union.edu>
2024-02-09 00:27:19 +0000

    changed name

--- modules/display/display.cpp
+++ modules/display/display.cpp
@@ -31,10 +31,10 @@
 #define DISPLAY_IR_FUNCTION_SET_5x10DOTS 0b00000100
 #define DISPLAY_IR_FUNCTION_SET_5x8DOTS  0b00000000
 
-#define DISPLAY_20x4_LINE1_FIRST_CHARACTER_ADDRESS 0
-#define DISPLAY_20x4_LINE2_FIRST_CHARACTER_ADDRESS 64
-#define DISPLAY_20x4_LINE3_FIRST_CHARACTER_ADDRESS 20
-#define DISPLAY_20x4_LINE4_FIRST_CHARACTER_ADDRESS 84
+#define DISPLAY_16x2_LINE1_FIRST_CHARACTER_ADDRESS 0
+#define DISPLAY_16x2_LINE2_FIRST_CHARACTER_ADDRESS 64
+#define DISPLAY_16x2_LINE3_FIRST_CHARACTER_ADDRESS 20
+#define DISPLAY_16x2_LINE4_FIRST_CHARACTER_ADDRESS 84
 
 #define DISPLAY_RS_INSTRUCTION 0
 #define DISPLAY_RS_DATA        1
@@ -140,7 +140,7 @@ void displayCharPositionWrite( uint8_t charPositionX, uint8_t charPositionY )
         case 0:
             displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
                               DISPLAY_IR_SET_DDRAM_ADDR |
-                              ( DISPLAY_20x4_LINE1_FIRST_CHARACTER_ADDRESS +
+                              ( DISPLAY_16x2_LINE1_FIRST_CHARACTER_ADDRESS +
                                 charPositionX ) );
             delay( 1 );         
         break;
@@ -148,7 +148,7 @@ void displayCharPositionWrite( uint8_t charPositionX, uint8_t charPositionY )
         case 1:
             displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
                               DISPLAY_IR_SET_DDRAM_ADDR |
-                              ( DISPLAY_20x4_LINE2_FIRST_CHARACTER_ADDRESS +
+                              ( DISPLAY_16x2_LINE2_FIRST_CHARACTER_ADDRESS +
                                 charPositionX ) );
             delay( 1 );         
         break;
@@ -156,7 +156,7 @@ void displayCharPositionWrite( uint8_t charPositionX, uint8_t charPositionY )
         case 2:
             displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
                               DISPLAY_IR_SET_DDRAM_ADDR |
-                              ( DISPLAY_20x4_LINE3_FIRST_CHARACTER_ADDRESS +
+                              ( DISPLAY_16x2_LINE3_FIRST_CHARACTER_ADDRESS +
                                 charPositionX ) );
             delay( 1 );         
         break;
@@ -164,7 +164,7 @@ void displayCharPositionWrite( uint8_t charPositionX, uint8_t charPositionY )
         case 3:
             displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
                               DISPLAY_IR_SET_DDRAM_ADDR |
-                              ( DISPLAY_20x4_LINE4_FIRST_CHARACTER_ADDRESS +
+                              ( DISPLAY_16x2_LINE4_FIRST_CHARACTER_ADDRESS +
                                 charPositionX ) );
             delay( 1 );         
         break;
